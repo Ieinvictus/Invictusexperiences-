74d440e47907fa19b6a356b26a8532df8d8d2e81
Invictusexperiences <rahulbpadaliya@zohomail.in>
2026-06-20 20:44:03 +0530

    Update script.js

--- privacy-policy/script.js
+++ privacy-policy/script.js
@@ -16,6 +16,27 @@ if (rmBarMenuBtn && rmBarCurtainMenu) {
   });
 
 }
+
+//terms card effect//
+const cards = document.querySelectorAll(".term-card");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  },
+  {
+    threshold: 0.2
+  }
+);
+
+cards.forEach((card) => {
+  observer.observe(card);
+});
+
 /* ===============================
 FOOTER SUBSCRIBE SYSTEM
 ================================ */
