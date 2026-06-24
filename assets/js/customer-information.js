9a613a90c0f39e78eb027444a814a1acaa13c89d
Invictusexperiences <rahulbpadaliya@zohomail.in>
2026-06-24 23:22:06 +0530

    Update customer-information.js

--- assets/js/customer-information.js
+++ assets/js/customer-information.js
@@ -100,7 +100,7 @@ await response.json();
 console.log(result);
 
 window.location.href =
-"https://invictusexperiences.com/assets/payment/";
+"https://invictusexperiences.com/assets/payment";
 
 } catch(error){
 
